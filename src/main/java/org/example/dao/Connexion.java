97dae79856c4b0c1980b6de9b25e2f54d37ebd1f
usernzl <unaizaab@gmail.com>
2026-09-19 23:43:05 -0400

    modifier connexion bd

--- src/main/java/org/example/dao/Connexion.java
+++ src/main/java/org/example/dao/Connexion.java
@@ -1,36 +1,17 @@
 package org.example.dao;
 
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class Connexion {
+    private static final String URL =
+            "jdbc:postgresql://localhost:5432/spotify_db";
+    private static final String USER = "postgres";
+    private static final String PASS = "Unaiza1231!";
 
     private Connexion() {}
-
     public static Connection getConnexion() throws SQLException {
-
-        Properties properties = new Properties();
-
-        try (InputStream input =
-                     Connexion.class.getResourceAsStream("/database.properties")) {
-
-            if (input == null) {
-                throw new SQLException("Fichier database.properties introuvable");
-            }
-
-            properties.load(input);
-
-        } catch (Exception e) {
-            throw new SQLException("Erreur de configuration", e);
-        }
-
-        String url = properties.getProperty("db.url");
-        String user = properties.getProperty("db.user");
-        String password = properties.getProperty("db.password");
-
-        return DriverManager.getConnection(url, user, password);
+        return DriverManager.getConnection(URL, USER, PASS);
     }
 }
